8cb5450bfa2d02683873f81c1d954a237c21b17c
jables-6384 <jables@live.fr>
2020-12-07 17:13:01 +0100

    add font awasome to icon module

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,12 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, FontAwesomeModule],
 })
-export class IconsModule { }
+export class IconsModule {}
