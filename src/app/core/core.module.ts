0f62be5fecf3963775a3ee757566ff6a4a5a8e54
jables-6384 <jables@live.fr>
2020-12-07 16:05:57 +0100

    archi ok

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { IconsModule } from '../icons/icons.module';
+import { LoginModule } from '../login/login.module';
+import { TextsModule } from '../texts/texts.module';
+import { UiModule } from '../ui/ui.module';
 
 
 
@@ -7,6 +11,13 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    UiModule,
+    LoginModule,
+    TextsModule,
+    IconsModule
+
   ]
 })
 export class CoreModule { }
