d58773180729a5c35ffdd9fc58ca5b98e6bd5566
jables-6384 <jables@live.fr>
2020-12-08 12:38:28 +0100

    ui component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,10 +5,22 @@ import { IconsModule } from '../icons/icons.module';
 import { LoginModule } from '../login/login.module';
 import { TextsModule } from '../texts/texts.module';
 import { UiModule } from '../ui/ui.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, NavComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [UiModule, LoginModule, TextsModule, IconsModule, NgbModule],
+  exports: [
+    UiModule,
+    LoginModule,
+    TextsModule,
+    IconsModule,
+    NgbModule,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+  ],
 })
 export class CoreModule {}
