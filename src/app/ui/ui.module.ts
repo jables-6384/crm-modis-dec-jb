097417556079144000571375dabc79b86cc2accb
jables-6384 <jables@live.fr>
2020-12-08 16:05:27 +0100

    ui2 + close nav

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiComponent } from './ui/ui.component';
+import { Ui2Component } from './ui2/ui2.component';
 
 
 
 @NgModule({
-  declarations: [UiComponent],
+  declarations: [UiComponent, Ui2Component],
   imports: [
     CommonModule
   ],
-  exports: [UiComponent]
+  exports: [UiComponent, Ui2Component]
 })
 export class UiModule { }
